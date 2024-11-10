7831c4190e78e95cbbfca5ddf3a808dd626c315f
Komrad71 <an.vasichkin@gmail>
2024-11-11 00:54:53 +0200

    npm install simplelightbox + js Done

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+function getFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    input.value = parsedData.email || '';
+    textarea.value = parsedData.message || '';
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+  }
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
